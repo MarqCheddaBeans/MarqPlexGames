d8a7128b0c998afc1697c21e9b724b3b0d32385b
MarqCheddaBeans <isaiahmv27@yahoo.com>
2025-12-13 13:49:50 -0500

    Add methods implementing CRUD functionality to shoppingcart

--- src/main/java/org/yearup/data/ShoppingCartDao.java
+++ src/main/java/org/yearup/data/ShoppingCartDao.java
@@ -1,12 +1,16 @@
 package org.yearup.data;
 
+import org.yearup.models.Product;
 import org.yearup.models.ShoppingCart;
-
-import java.util.List;
+import org.yearup.models.ShoppingCartItem;
 
 public interface ShoppingCartDao
 {
     ShoppingCart getByUserId(int userId);
     // add additional method signatures here
+    void addToCart(int productID, int userID);
 
+    void clearCart(int userID);
+
+    public void editCart(int productID, int userID,int quantity);
 }
--- src/main/java/org/yearup/data/mysql/MySqlShoppingCartDao.java
+++ src/main/java/org/yearup/data/mysql/MySqlShoppingCartDao.java
@@ -1,5 +1,6 @@
 package org.yearup.data.mysql;
 
+import org.apache.ibatis.jdbc.SQL;
 import org.springframework.stereotype.Component;
 import org.yearup.data.ShoppingCartDao;
 import org.yearup.models.Product;
@@ -12,7 +13,6 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.List;
 
 @Component
 public class MySqlShoppingCartDao extends MySqlDaoBase implements ShoppingCartDao {
@@ -57,6 +57,81 @@ public class MySqlShoppingCartDao extends MySqlDaoBase implements ShoppingCartDa
             System.out.println("Error getting cart by userid " + e);
         }
         return cart;
+    }
+
+    @Override
+    public void addToCart(int productId, int userID) {
+        try(
+                Connection c = ds.getConnection();
+                //prepare statement to UPSERT product to cart accounting for multiple of same products
+                PreparedStatement q = c.prepareStatement("""
+                        INSERT INTO shopping_cart (user_id, product_id,quantity) VALUES (?,?,1)
+                        ON DUPLICATE KEY UPDATE quantity = quantity + 1
+                        """)
+                ){
+            q.setInt(1, userID);
+            q.setInt(2, productId);
+
+            q.executeUpdate();
+        }catch(SQLException e){
+            System.out.println("Error adding to cart" + e);
+        }
+    }
+
+    @Override
+    public void clearCart(int userID) {
+        try(
+                Connection c = ds.getConnection();
+                PreparedStatement q = c.prepareStatement("""
+                        DELETE FROM shopping_cart
+                        WHERE user_id = ?
+                        """)
+                ){
+            q.setInt(1, userID);
+
+            q.executeUpdate();
+        }catch(SQLException e){
+            System.out.println("Error clearing cart " + e);
+        }
+    }
+
+    @Override
+    public void editCart(int productID, int userID, int quantity) {
+        //1st query to account for multiple of same item in cart update quantity
+        try(
+                Connection c = ds.getConnection();
+                PreparedStatement q = c.prepareStatement("""
+                        UPDATE shopping_cart
+                        SET quantity = ?
+                        WHERE user_id = ?
+                        AND product_id = ?
+                        AND quantity >= 1
+                        """)
+                ){
+            q.setInt(1,quantity);
+            q.setInt(2, userID);
+            q.setInt(3, productID);
+
+            q.executeUpdate();
+
+            //After updating, if quantity is 0, remove product from cart
+            if(quantity == 0){
+                try (
+                        PreparedStatement rQ = c.prepareStatement("""
+                                DELETE FROM shopping_cart
+                                WHERE quantity = 0
+                                """)
+                        ){
+                    rQ.executeUpdate();
+
+                }catch(SQLException e){
+                    System.out.println("Error removing item" + e);
+                }
+            }
+
+        }catch(SQLException e){
+            System.out.println("Error removing from cart" + e);
+        }
     }
 
     protected static Product mapRow(ResultSet row) throws SQLException {
